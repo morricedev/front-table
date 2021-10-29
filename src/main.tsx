bf9316755992eb26da8598bc29f3e207aa13e91d
Maurício Sousa <morricedev@gmail.com>
2021-10-29 15:16:15 -0300

    Add EmployeeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
+
 import App from "./App";
+import { EmployeeProvider } from "./contexts/employee";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <EmployeeProvider>
+      <App />
+    </EmployeeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
