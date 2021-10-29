3d585259a6c9db9f2c7089d6505b29d85530df89
Maurício Sousa <morricedev@gmail.com>
2021-10-29 15:16:44 -0300

    Finish home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,19 @@
 import "./App.css";
+import { EmployeesList } from "./components/EmployeesList";
+
+import { Header } from "./components/Header";
+import { SearchContainer } from "./components/SearchContainer";
 
 function App() {
-  return <h1>Oi</h1>;
+  return (
+    <>
+      <Header />
+      <main>
+        <SearchContainer />
+        <EmployeesList />
+      </main>
+    </>
+  );
 }
 
 export default App;
