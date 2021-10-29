3b1ff93b72145d6101f016b65d511320fc7161fe
Maurício Sousa <morricedev@gmail.com>
2021-10-29 15:15:45 -0300

    Add handleSearch &button to reset search

--- src/components/SearchContainer/index.tsx
+++ src/components/SearchContainer/index.tsx
@@ -1,18 +1,52 @@
-import { FaSearch } from "react-icons/fa";
+import { useRef } from "react";
+import { FaSearch, FaTimes } from "react-icons/fa";
+import { useEmployee } from "../../hooks/useEmployee";
 
 import "./style.css";
 
 export function SearchContainer() {
+  const searchInput = useRef<HTMLInputElement | null>(null);
+  const { isLoading, searchEmployees, fetchEmployees } = useEmployee();
+
+  const handleSearch = () => {
+    const inputValue = searchInput.current?.value;
+    if (!inputValue) return;
+
+    searchEmployees(inputValue);
+  };
+
+  const handleResetSearch = () => {
+    if (searchInput.current) {
+      searchInput.current.value = "";
+    }
+
+    fetchEmployees();
+  };
+
   return (
     <div className="searchContainer">
       <h1>Funcionários</h1>
 
       <div className="search">
-        <input type="text" placeholder="Pesquisar" />
+        <input ref={searchInput} type="text" placeholder="Pesquisar por nome" />
 
-        <button className="submitSearch">
+        <button
+          disabled={isLoading}
+          className="submitSearch"
+          onClick={handleSearch}
+        >
           <FaSearch />
         </button>
+
+        {searchInput.current?.value && (
+          <button
+            disabled={isLoading}
+            className="resetSearch"
+            onClick={handleResetSearch}
+          >
+            <FaTimes />
+          </button>
+        )}
       </div>
     </div>
   );
