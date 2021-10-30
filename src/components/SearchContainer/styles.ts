ab7710c20f09e3e22eaa2e42715212dc88d16416
Maurício Sousa <morricedev@gmail.com>
2021-10-29 22:31:18 -0300

    Change input buttons size

--- src/components/SearchContainer/styles.ts
+++ src/components/SearchContainer/styles.ts
@@ -29,7 +29,7 @@ export const Search = styled.div`
 export const SearchInput = styled.input`
   ${({ theme }) => css`
     width: 28.2rem;
-    padding: 1.2rem 4.5rem 1.2rem 1.2rem;
+    padding: 1.2rem 5rem 1.2rem 1.2rem;
     background-color: ${theme.colors.white};
     border: 0.1rem solid ${theme.colors.gray200};
     border-radius: 0.4rem;
@@ -44,8 +44,8 @@ export const SearchInput = styled.input`
 export const SearchButton = styled.button`
   ${({ theme }) => css`
     position: absolute;
-    top: 11%;
-    right: 2rem;
+    top: 9%;
+    right: 0.3rem;
     z-index: 10;
     border: none;
     background-color: transparent;
@@ -58,10 +58,15 @@ export const SearchButton = styled.button`
     &:disabled {
       cursor: not-allowed;
     }
+
+    svg {
+      width: 17px;
+      height: 17px;
+    }
   `}
 `;
 
 export const ResetSearchButton = styled(SearchButton)`
-  top: 14%;
-  right: 0.3rem;
+  top: 8%;
+  right: 2.5rem;
 `;
